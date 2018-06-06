8fafb56edb624ee211ab640c503e8295bb3e43a4
Harpreet Singh <harpreet.singh@globant.com>
2018-06-06 13:48:16 +0530

    initial server setup

--- src/env.js
+++ src/env.js
@@ -6,5 +6,5 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-console.log("patth------", path.resolve(__dirname, '.env'));
-dotenv.config({path: path.resolve(__dirname, '.env')});
+console.log("patth------", path.resolve(__dirname, '../.env'));
+dotenv.config({path: path.resolve(__dirname, '../.env')});
